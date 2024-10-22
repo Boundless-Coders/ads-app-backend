6a295e2e696134358319cd59473724a535662d84
Frank Koomson <frankkoomson1@gmail.com>
2024-10-22 11:10:48 +0000

    Changesd vendor to user in the advert model

--- models/advert.js
+++ models/advert.js
@@ -2,12 +2,12 @@ import { Schema, model } from "mongoose";
 import { toJSON } from "@reis/mongoose-to-json";
 
 const advertSchema = new Schema({
-    title: { type: String, required: true }, // Title of the advert
-    description: { type: String}, // Description of the advert
-    price: { type: Number}, // Price of the product/service
-    category: { type: String}, // Category the advert falls under
-    image: { type: String}, // URL or path to the image
-    vendor: { type: Schema.Types.ObjectId, required: true, ref: 'User'} 
+    title: { type: String, required: true }, 
+    description: { type: String}, 
+    price: { type: Number}, 
+    category: { type: String}, 
+    image: { type: String}, 
+    user: { type: Schema.Types.ObjectId, required: true, ref: 'User'} 
     // Reference to the user who posted the advert
 }, {
     timestamps: true,

3a4f41aa73d2f7e96d71167965ed758b44a6e88f
Frank Koomson <frankkoomson1@gmail.com>
2024-10-23 10:32:05 +0000

    deleted business name and contact number in the user model

--- models/user.js
+++ models/user.js
@@ -4,10 +4,8 @@ import { toJSON } from "@reis/mongoose-to-json";
 
 const userSchema = new Schema({
     name: { type: String, required: true },
-    businessName: {type: String},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    contactNumber: {type: Number, required: true},
     role: { type: String, default: 'user', enum: ['user', 'vendor'] },
 }, {
     timestamps: true,

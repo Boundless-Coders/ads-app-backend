4a29d140d1995b2c8bf3281428d670e28e5d7c1b
Frank Koomson <frankkoomson1@gmail.com>
2024-10-24 15:20:50 +0000

    added new end points

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,7 @@ import { UserModel } from "../models/user.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { mailTransporter } from "../utils/mails.js";
-
+import { AdvertModel } from "../models/advert.js";
 // Register user (vendor or regular)
 export const registerUser = async (req, res, next) => {
     try {
@@ -71,6 +71,47 @@ export const getProfile = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getUserAdverts = async (req, res, next) => {
+    try {
+        // Extract query params
+        const { title, category, minPrice, maxPrice, limit = 10, skip = 0, sort = "{}" } = req.query;
+        let filter = {};
+
+        // If title query param exists, perform a case-insensitive search on the title field
+        if (title) {
+            filter.title = { $regex: title, $options: 'i' }; // 'i' for case-insensitive
+        }
+
+        // If category query param exists, add it to the filter
+        if (category) {
+            filter.category = category;
+        }
+
+        // If minPrice or maxPrice query params exist, filter by price range
+        if (minPrice || maxPrice) {
+            filter.price = {};
+            if (minPrice) filter.price.$gte = minPrice;
+            if (maxPrice) filter.price.$lte = maxPrice;
+        }
+
+        // Fetch adverts from the database based on filter, with pagination and sorting
+        const adverts = await AdvertModel
+            .find({
+                ...filter,
+                user: req.auth.id
+            })
+            .sort(JSON.parse(sort)) // Sort by the provided sort query
+            .limit(Number(limit))   // Limit number of results
+            .skip(Number(skip));    // Skip the first n results for pagination
+
+        // Respond with the list of adverts
+        res.status(200).json(adverts);
+    } catch (error) {
+        next(error);
+
+    }
+}
 
 // Update user profile
 export const updateProfile = async (req, res, next) => {

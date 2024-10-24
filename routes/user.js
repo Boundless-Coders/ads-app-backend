4c363e650ec2d4e58b42e05d8f22625ff6ca2546
Frank Koomson <frankkoomson1@gmail.com>
2024-10-24 22:48:53 +0000

    vkjvj

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,24 @@
 import { Router } from "express";
-import { registerUser, loginUser, getProfile, updateProfile, getUserAdverts, deleteUserProfile } from "../controllers/user.js";
+import { registerUser, loginUser, getProfile, updateProfile, getUserAdverts } from "../controllers/user.js";
 import { userAvatarUpload } from "../middlewares/upload.js";
 import { isAuthenticated, hasPermission } from "../middlewares/auth.js";
 
 // Create a router
 const userRouter = Router();
 
-
+// Define routes for user operations
+// Route to register a new user
 userRouter.post('/users/register', registerUser);
+
+// Route to login a user
 userRouter.post('/users/login', loginUser);
+
+// Route to get the logged-in user's profile (must be authenticated)
 userRouter.get('/users/me', isAuthenticated, getProfile);
+
 userRouter.get('/users/me/adverts', isAuthenticated, getUserAdverts);
-userRouter.delete('/delete', isAuthenticated, hasPermission('delete_profile'), deleteUserProfile) 
+
+// Route to update the user's profile (must be authenticated)
 userRouter.patch('/users/me', isAuthenticated, hasPermission('update_profile'), userAvatarUpload.single('avatar'), updateProfile);
 
 // Export router
